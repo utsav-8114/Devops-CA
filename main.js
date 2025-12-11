d2ed43e1538b2bb596406026d82c9d58b3166f81
Utsav Mukherjee <utsavmukherjee12@gmail.com>
2025-12-11 16:29:53 +0530

    final commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World this is updated code after ci/cd integration!');
+  res.send('hi iam here to prove my pipeline works!');
 });
 
 app.listen(port, () => {
